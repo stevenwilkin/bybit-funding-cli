f1e14b35df2a11c88ec61c20c2eec413479b9bd2
Steven Wilkin <stevenwilkin@gmail.com>
2023-08-13 19:39:52 +0700

    Fetch Bybit funding rate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use serde::Deserialize;
+
+#[derive(Deserialize,Debug)]
+struct Payload {
+    result: Result,
+}
+
+#[derive(Deserialize,Debug)]
+struct Result {
+    list: Vec<Ticker>,
+}
+
+#[derive(Deserialize,Debug)]
+#[serde(rename_all = "camelCase")]
+struct Ticker {
+    #[serde(default)]
+    funding_rate: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let url = reqwest::Url::parse_with_params(
+        "https://api.bybit.com/v5/market/tickers",
+        &[("category", "inverse"), ("symbol", "BTCUSD")]
+    ).expect("Can't generate URL");
+
+    let res = reqwest::blocking::get(url).expect("Failed to GET url");
+    let payload: Payload = serde_json::from_reader(res).expect("Failed to parse response");
+
+    if payload.result.list.len() != 1 {
+        eprintln!("Expected a single element");
+    }
+
+    let funding: f32 = payload.result.list[0].funding_rate.parse().expect("Failed to parse funding rate");
+
+    println!("{:?}%", funding * 100.0);
 }
